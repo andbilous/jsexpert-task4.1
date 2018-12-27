607c8a60d12042a4b9b4f0842bce972148412707
andbilous <andbilous@gmail.com>
2018-12-27 16:59:41 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+"use strict";
+
+var total = 0;
+var result = "";
+
+function getRndNumber(){
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+function setResult(input){
+  result += input;
+  return result;
+}
+
+function isNumbersEqual (firstNum,secondNum){
+      if(firstNum===secondNum){
+         setResult("Выпал дубль. Число " + firstNum+"<br>")
+      } 
+}
+
+function isBigDifference (firstNum,secondNum){
+if (Math.abs(firstNum-secondNum)>2) {
+    setResult("Большой разброс между костями. Разница составляет : " + (secondNum - firstNum)+"<br>") 
+  }
+  
+}
+
+function calculateResult(firstNum,secondNum){
+total = firstNum + secondNum;
+}
+
+function printResult(){
+  
+  document.getElementById("result").innerHTML=result;
+}
+
+function addResult(){
+  setResult("Победа, вы набрали "+total+" очков")
+} 
+
+
+(function run(){
+for (var i = 0; i < 15; i++) {
+  if (i === 8 || i === 13) {
+    continue;
+  }
+
+  var first = getRndNumber();
+  var second = getRndNumber();
+
+  isNumbersEqual(first,second);
+  isBigDifference(first,second);
+  calculateResult(first,second);
+  setResult(" Первая кость: " + first+"<br>" + " Вторая кость: " + second + "<br>")
+
+  
+}
+addResult()
+printResult()
+
+})()
+
